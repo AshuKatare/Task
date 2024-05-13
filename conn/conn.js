a5d7d3276e124f443ab2279d9e3766ba56b5b2fa
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-13 13:54:49 +0530

    first commit

--- conn/conn.js
+++ conn/conn.js
@@ -4,7 +4,7 @@ const conn = async (req, res) => {
   try {
     await mongoose
       .connect(
-        ""
+        "mongodb+srv://sandeep18:san72111@cluster0.ueh8yll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
       )
       .then(() => {
         console.log("Connected");
